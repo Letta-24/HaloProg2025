152d4f91c2711e96262872f92e82f3cca310f54d
Letta-24 <gletti206@gmail.com>
2025-09-16 10:17:54 +0200

    ujitva

--- elso.py
+++ elso.py
@@ -1,28 +1,39 @@
 import random
 
-#Lista létrehozása
-szamok = []
-#A JÁTÉK -------------------------------
-kitalalando_szam = 12
+szamok=[]
 
-jatszol = True
-while(jatszol):
-    tipp_sz = input("Tipped? (egész szám):")
-    if(tipp_sz.isdecimal()):
-        tipp = int(tipp_sz)
-    else:
-        print("Egész számmal játsz!")
-        continue
-    while(tipp != kitalalando_szam):
-        tipp = int(input("Tipped? (egész szám):"))
-        tipp_sz = input("Tipped? (egész szám):")
-    if(tipp_sz.isdecimal()):
-        tipp = int(tipp_sz)
-    else:
-        print("Egész számmal játsz!")
-        continue
-    print("Kitaláltad a számot!")
+#lista feltoltese 100db random 2jegyu egesz szammal
+for i in range (100):
+    szam=random.randint(10,99)
+    szamok.append(szam)
     
-    folytatas = input("Akarsz-e még játszani [I/N]?")
-    if folytatas == "N":
-        jatszol = False
+#ell
+print(szamok) 
+
+#EGYSZÁM JáTÉK
+jatek_szam=0
+nem_talalDB=0
+
+kitalalando_szam=szamok[random.randint(0, len(szamok))]
+jatszol=True
+while (jatszol):
+    tipp_sz=input("tipped?: ").strip()
+    if (tipp_sz.isdecimal()):
+        tipp=int(tipp_sz)
+    else:
+        print("egész számmal játsz")
+        continue
+    
+    while (tipp!=kitalalando_szam):
+        tipp_sz=input("tipped?: ").strip()
+        if (tipp_sz.isdecimal()):
+            tipp=int(tipp_sz)
+        else:
+            print("egész számmal játsz")
+            continue
+        
+    print("eltaláltad")
+    
+    folytat=input("akarsz meg jatszani? [I/N]")
+    if (folytat=="N"):
+        jatszol=False
